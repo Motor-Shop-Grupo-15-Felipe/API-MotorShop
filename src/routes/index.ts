3edeb1a5a9f35f786c2e3a085cb96395d59fdf76
terragabriella <gabriellaterra8@gmail.com>
2023-02-21 16:47:24 -0300

    feat:migrations user created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,12 @@
 import { Express } from "express"
 import { routesAnnoucements } from "./adds/announcement.routes"
-<<<<<<< HEAD
 import { sessionRouter } from "./sesssions/sessions.routes"
 import { usersRouter } from "./users/users.routes"
 
 
 export const appRoutes = (app: Express) => {
   app.use("/annoucements", routesAnnoucements())
-  //app.use("/users", usersRouter()),
+  app.use("/users", usersRouter()),
   app.use("/login",sessionRouter())
   
-=======
-import { usersRouter } from "./users/users.routes"
-
-export const appRoutes = (app: Express) => {
-  app.use("/announcements", routesAnnoucements())
-  app.use("/user", usersRouter())
-
->>>>>>> 21410c4a3a7d677c2d46f3727753005ca611c9e2
 }
