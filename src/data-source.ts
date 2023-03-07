54503e6bb4d025e5ee496c0a34bf46382ac28fff
terragabriella <gabriellaterra8@gmail.com>
2023-03-07 16:37:56 -0300

    fix:deploy data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,26 +1,45 @@
 import { DataSource } from "typeorm"
 import "dotenv/config"
+import path from "path";
 
-const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"],
-      }
-    : {
-        type: "postgres",
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PWD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
-        synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      }
-)
+const setDataSourceConfig = (): any => {
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{js,ts}"
+  );
 
-export default AppDataSource
+
+const nodeEnv = process.env.NODE_ENV;
+
+  if(nodeEnv === "production"){
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    }
+  }
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PWD,
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    synchronize: false,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
